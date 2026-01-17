87cf150db13dbe9f119095ba4c537269b44c96c4
betoniochristian94-maker <betoniochristian94@gmail.com>
2026-01-17 21:59:45 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,11 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('cashback-cache').then(cache => cache.addAll([
-      '/',
-      '/index.html',
-      '/manifest.json',
-      '/app.js'
-    ]))
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+self.addEventListener("activate", () => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
