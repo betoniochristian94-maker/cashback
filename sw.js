2e2f77a6879f9377b557b9bdafba08a0d08f70cb
betoniochristian94-maker <betoniochristian94@gmail.com>
2026-01-18 14:54:53 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,30 @@
-self.addEventListener("install", () => {
-  self.skipWaiting();
+const cacheName = "cashbacker-cache-v1";
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
-self.addEventListener("fetch", () => {});
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
