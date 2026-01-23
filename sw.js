1092d57b673f90af2d6322b2c05afbcf275a4233
betoniochristian94-maker <betoniochristian94@gmail.com>
2026-01-23 23:27:03 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,18 +9,22 @@ const assetsToCache = [
   "./icon-512.png"
 ];
 
+// INSTALL
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(cacheName).then(cache => cache.addAll(assetsToCache))
   );
 });
 
+// ACTIVATE
 self.addEventListener("activate", event => {
   event.waitUntil(self.clients.claim());
 });
 
+// FETCH
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request)
+      .then(res => res || fetch(event.request).catch(() => caches.match("./index.html")))
   );
 });
